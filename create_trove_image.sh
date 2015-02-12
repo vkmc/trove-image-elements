456504c3aeeeeab42e09d6b3ce06cc09d532ae14
Victoria Martínez de la Cruz <victoria@redhat.com>
2015-02-12 08:48:50 -0300

    Add systemctl enable/start for mariadb in the mysql element Fix the trove-guest element for RHEL and CentOS Refactors elements order. This adds code duplication but its needed to ensure a correct creation of images.

--- create_trove_image.sh
+++ create_trove_image.sh
@@ -2,19 +2,19 @@
 
 set -e
 
-export DISTRO=rhel
+export DISTRO=fedora
 export DATASTORE=mysql
 
-export DIB_REPO_PATH="/opt/stack/diskimage-builder"
+export DIB_REPO_PATH="/usr/share/diskimage-builder"
 export ELEMENTS_PATH="$DIB_REPO_PATH/elements:./elements"
 
-export REG_METHOD=portal
-export REG_USER=<user>
-export REG_PASSWORD=<password>
-export REG_MACHINE_NAME=trove
-export REG_POOL_ID=<pool>
+# export REG_METHOD=portal
+# export REG_USER=<user>
+# export REG_PASSWORD=<password>
+# export REG_MACHINE_NAME=trove
+# export REG_POOL_ID=<pool>
 
-export DIB_LOCAL_IMAGE=<localimg>
+export DIB_LOCAL_IMAGE=/home/vkmc/Fedora-Cloud-Base-20141203-21.x86_64.qcow2
 export DIB_CLOUD_INIT_DATASOURCES="ConfigDrive"
 
 disk-image-create -a amd64 -o ${DISTRO}-${DATASTORE}-guest-image -x --qemu-img-options compat=0.10  ${DISTRO}-${DATASTORE}-guest-image
